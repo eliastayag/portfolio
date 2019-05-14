fa6aed02055e02d5f8fa144814d1e61cfa78a204
eliastayag <43620633+eliastayag@users.noreply.github.com>
2019-05-14 00:18:30 -0700

    x

---
+++ graphic design page/app.js
@@ -0,0 +1,12 @@
+function gameboy(){
+    document.getElementById("gameboy").style.width = "50vw";
+    document.getElementById("gameboy").style.height = "70vh";
+    document.getElementById("gamebut1").style.visibility = "visible";
+}
+
+function gameboymin(){
+    document.getElementById("gameboy").style.width = "35vw";
+    document.getElementById("gameboy").style.height = "50vh";
+    document.getElementById("gamebut1").style.visibility = "hidden";
+
+}
---
+++ video editing page/app.js
@@ -0,0 +1,13 @@
+function phone(){
+    document.getElementById("phone").style.left = "30%" ;
+    document.getElementById("phone").style.top = "35%" ;
+    document.getElementById("green").style.left = "-500px" ;
+
+}
+
+function green(){
+    document.getElementById("green").style.left = "30%" ;
+    document.getElementById("green").style.top = "35%" ;
+    document.getElementById("phone").style.left = "-500px" ;
+
+}
